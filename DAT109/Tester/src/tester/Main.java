67914e649e3c35c2e35d765cc56993b2e8174304
RS124810 <69851083+RS124810@users.noreply.github.com>
2022-02-08 15:25:01 +0100

    Add files via upload
    Start

---
+++ DAT109/Tester/src/tester/Main.java
@@ -0,0 +1,12 @@
+package tester;
+
+import javax.swing.JOptionPane;
+
+public class Main {
+
+	public static void main(String[] args) {
+	
+		BrukerGrensesnitt meny = new BrukerGrensesnitt();
+		meny.start();
+	}
+}
